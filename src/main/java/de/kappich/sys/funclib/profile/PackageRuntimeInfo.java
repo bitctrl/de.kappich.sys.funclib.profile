d711c925a9e3706fac5e6a2dc7ed720396225a25
Uwe Peuker <dev@upeuker.net>
2017-07-23 17:12:42 +0200

    Übernahme Version 3.9.2

--- src/main/java/de/kappich/sys/funclib/profile/PackageRuntimeInfo.java
+++ src/main/java/de/kappich/sys/funclib/profile/PackageRuntimeInfo.java
@@ -2,10 +2,10 @@
             package de.kappich.sys.funclib.profile;
             public class PackageRuntimeInfo {
             public static String getLicence() { return "GPL";}
-            public static String getRelease() { return "Kernsoftware 3.9.1";}
-            public static String getVersion() { return "3.9.1";}
-            public static String getRevision() { return "34e82dc532c8d9bd6c65dd9b7ced5ccd7b031482";}
-            public static String getCompileTime() { return "03.08.2016 20:24:59";}
+            public static String getRelease() { return "Kernsoftware 3.9.2";}
+            public static String getVersion() { return "3.9.2";}
+            public static String getRevision() { return "866ec7f8528df5f1c8965c1d289385ec3737472a";}
+            public static String getCompileTime() { return "26.05.2017 11:27:22";}
             public static String getDependsOnCompiled() { return "";}
             public static String getDependsOnSource() { return "";}
             public static String getDependsOnLib() { return "";}
